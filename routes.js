67f7de12db6194ddb615990aff77b14917d9c34b
Aadil Glasswala <aglasswala@gmail.com>
2019-03-06 00:34:55 -0500

    updating routes'

--- routes.js
+++ routes.js
@@ -4,31 +4,31 @@ const eventController = require('./controllers/eventController')
 
 module.exports = (router) => {
 
-    // This will load when the front page loads
-    router.get('/', (req, res) => res.status(200).send("OH YEAH"));
+    // // This will load when the front page loads
+    // router.get('/', (req, res) => res.status(200).send("OH YEAH"));
 
-    // TICKETS
-    router.get('/tickets', ticketController.getTicket) // GET all tickets
+    // // TICKETS
+    // router.get('/tickets', ticketController.getTicket) // GET all tickets
 
-    router.post('/tickets', ticketController.buyTicket) // POST new Ticket
+    // router.post('/tickets', ticketController.buyTicket) // POST new Ticket
 
-    router.put('/tickets', ticketController.updateTicket) // Update Ticket 
+    // router.put('/tickets', ticketController.updateTicket) // Update Ticket 
 
-    // EVENTS
-    router.get('/events', eventController.getAllEvents) // GET all events
+    // // EVENTS
+    // router.get('/events', eventController.getAllEvents) // GET all events
 
-    router.get('/events/:eventId', eventController.getEventById) // GET events by Id
+    // router.get('/events/:eventId', eventController.getEventById) // GET events by Id
 
-    router.post('/events', eventController.createEvent) // POST new Event
+    // router.post('/events', eventController.createEvent) // POST new Event
 
-    // USERS
+    // // USERS
     router.post('/login', userController.loginUser) // Logs in user
 
     router.get('/getUserById', userController.getUserById)
 
-    router.get('/user/:userId/tickets', (req, res) => {
-        res.status(200).send("this is a better endpoint"); // GET user tickets by ID
-    })
+    // router.get('/user/:userId/tickets', (req, res) => {
+    //     res.status(200).send("this is a better endpoint"); // GET user tickets by ID
+    // })
 
-    router.delete('/users/:userId', userController.deleteUser) // Deletes User
+    // router.delete('/users/:userId', userController.deleteUser) // Deletes User
 };
