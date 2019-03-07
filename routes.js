aa055832188958368d3f13e4afe67ae92dba997a
Aadil Glasswala <aglasswala@gmail.com>
2019-03-07 14:42:24 -0500

    updating router

--- routes.js
+++ routes.js
@@ -5,26 +5,33 @@ const eventController = require('./controllers/eventController')
 module.exports = (router) => {
 
     // // This will load when the front page loads
-    // router.get('/', (req, res) => res.status(200).send("OH YEAH"));
+    router.get('/', (req, res) => res.status(200).send("OH YEAH"));
 
-    // // TICKETS
-    // router.get('/tickets', ticketController.getTicket) // GET all tickets
+    // TICKETS
+    router.get('/tickets', ticketController.getTicket) // GET all tickets
 
-    // router.post('/tickets', ticketController.buyTicket) // POST new Ticket
+    router.post('/tickets', ticketController.buyTicket) // POST new Ticket
 
-    // router.put('/tickets', ticketController.updateTicket) // Update Ticket 
+    router.put('/tickets', ticketController.updateTicket) // Update Ticket 
 
-    // // EVENTS
-    // router.get('/events', eventController.getAllEvents) // GET all events
+    // EVENTS
+    router.get('/events', eventController.getAllEvents) // GET all events
 
-    // router.get('/events/:eventId', eventController.getEventById) // GET events by Id
+    router.get('/events/:eventId', eventController.getEventById) // GET events by Id
 
-    // router.post('/events', eventController.createEvent) // POST new Event
+    router.post('/events', eventController.createEvent) // POST new Event
 
-    // // USERS
+    // Login information
     router.post('/login', userController.loginUser) // Logs in user
-
     router.get('/getUserById', userController.getUserById)
+
+    // Register information
+
+    router.post('/register', (req, res) => {
+        const { firstName } = req.body
+        console.log(firstName)
+        res.status(200).json(firstName)
+    })
 
     // router.get('/user/:userId/tickets', (req, res) => {
     //     res.status(200).send("this is a better endpoint"); // GET user tickets by ID
