a5700703757974c07da9cfa48366861444a49bc9
Aadil Glasswala <aglasswala@gmail.com>
2019-04-28 22:21:05 -0400

    updated db urls

--- services/events/eventService.js
+++ services/events/eventService.js
@@ -3,10 +3,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '1234',
-    database: 'postgres',
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   },
 });
 
